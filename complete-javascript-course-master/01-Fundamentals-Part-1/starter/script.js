74b2174f3f24de0d1ce0d751afa039924c87d3cb
James <takw14@gmail.com>
2021-06-09 15:58:00 +0800

    initial commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,40 @@
+/*
+let js = 'amazing';
+if (js == 'amazing') alert ('Javascript is FUN!')
+40+32+3+232;
+console.log(40+32+3+232);
+
+console.log ("Jonas");
+console.log (23);
+
+
+// variable name conventions
+let james_matilda = "JM";
+let $function = 27;
+
+let person = 'james'
+
+// console.log(person)
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun)
+
+// console.log(typeof 'james')
+
+javascriptIsFun = "Yes!"
+console.log(javascriptIsFun)
+
+/*
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now -2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas *2,ageJonas/10 );
+*/
+
+const firstName = 'James'
+const lastName = 'Morillo'
+
+console.log(firstName + '' + lastName)
+*/
