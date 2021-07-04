4e6629128d95e1e6e446866d50da278b2e41ad3e
James <takw14@gmail.com>
2021-07-04 16:40:34 +0800

    Update script.js

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -64,18 +64,86 @@
 // CODING CHALLENGE
 
 
-calcAverage = (param1, param2, param3) => (param1 + param2 + param3) / 3
+// calcAverage = (param1, param2, param3) => (param1 + param2 + param3) / 3
+//
+//
+// const dolphinAve = calcAverage(85,54,41);
+// const koalaAve = calcAverage(23, 34, 27);
+//
+// function checkWinner (dolphinAve, koalaAve) {
+//   if (koalaAve >= 2 * dolphinAve) {
+//     return `Koalas won (${koalaAve} vs ${dolphinAve})`
+//   } else if (dolphinAve >= 2*koalaAve) {
+//     return `dolphins won (${dolphinAve} vs ${koalaAve})`
+//   } else return `Nobody won. (${dolphinAve} vs ${koalaAve}) `
+// }
+//
+// console.log(checkWinner(dolphinAve, koalaAve));
 
+// ==============================================================================================================
 
-const dolphinAve = calcAverage(85,54,41);
-const koalaAve = calcAverage(23, 34, 27);
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+//
+// const friends = ['Michael', 'Steven', 'Peter']
+//
+// console.log(friends);
+//
+// const years = new Array (1991, 1973, 1882);
+//
+// console.log(years);
+// console.log(friends[0])
+// const firstName = 'James'
+// const james = [firstName, 'Morillo', 2037-1991, 'teacher', friends]
+// console.log(james);
+// console.log(james.length);
+//
+// Excercise
+//
+// const years = new Array (1991, 1973, 1882);
+//
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+//
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
 
-function checkWinner (dolphinAve, koalaAve) {
-  if (koalaAve >= 2 * dolphinAve) {
-    return `Koalas won (${koalaAve} vs ${dolphinAve})`
-  } else if (dolphinAve >= 2*koalaAve) {
-    return `dolphins won (${dolphinAve} vs ${koalaAve})`
-  } else return `Nobody won. (${dolphinAve} vs ${koalaAve}) `
-}
+// ==============================================================================================================
 
-console.log(checkWinner(dolphinAve, koalaAve));
+// const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay');
+// console.log(friends);
+//
+// friends.unshift('John');
+// console.log(friends);
+//
+// friends.pop();
+// console.log(friends);
+//
+// friends.shift();
+// console.log(friends);
+
+// ==============================================================================================================
+
+// CODING CHALLENGE
+//
+// myTip = function calcTip(bill) {
+//   if (bill >= 50 && bill <= 300) {
+//     return bill * 0.15
+//   } else {
+//     return bill * 0.2
+//   }
+// }
+//
+// bills = [125, 555, 44]
+//
+// tips = [myTip(bills[0]), myTip(bills[1]), myTip(bills[bills.length - 1])]
+// total = [125+tips[0], 555+tips[1], 44+tips[tips.length - 1]]
+// console.log(tips)
+// console.log(total)
+
+// ==============================================================================================================
