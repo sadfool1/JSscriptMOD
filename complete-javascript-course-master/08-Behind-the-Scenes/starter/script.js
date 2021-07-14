31544702f471524e37bcdb1ff6b5d0361e3813ce
James <takw14@gmail.com>
2021-07-14 15:46:39 +0800

    Hoisiting & TDZ

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,33 +1,49 @@
-'use strict';
+// 'use strict';
+//
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'steven';
+//       const str = `Oh and you're millenial, ${firstName}`;
+//       console.log(str);
+//     }
+//
+//     function add(a, b) {
+//       return a + b;
+//     }
+//
+//     output = 'New OUtput';
+//     console.log(output);
+//
+//     // console.log(add(2, 3));
+//     // console.log(str); // will not work as const is block scoped.
+//   }
+//
+//   printAge();
+//   return age;
+// }
+//
+// const firstName = 'James';
+// calcAge(1991);
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+var me = 'James';
+let job = 'teacher';
+const year = 1991;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// functions
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = 'steven';
-      const str = `Oh and you're millenial, ${firstName}`;
-      console.log(str);
-    }
-
-    function add(a, b) {
-      return a + b;
-    }
-
-    output = 'New OUtput';
-    console.log(output);
-
-    // console.log(add(2, 3));
-    // console.log(str); // will not work as const is block scoped.
-  }
-
-  printAge();
-  return age;
+function AddDEcl(a, b) {
+  return a + b;
 }
 
-const firstName = 'James';
-calcAge(1991);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
