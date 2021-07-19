8acc1143a13ad6cac6a424a33aceaca37ea4150a
James <takw14@gmail.com>
2021-07-19 19:56:01 +0800

    Update script.js

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,95 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+// ============================================================================================================
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Visa del sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+//
+// restaurant.orderDelivery({
+//   address: 'Visa del sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 1,
+// });
+// const arr = [2, 3, 4];
+//
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+//
+// const [x, y, z] = arr; // destructuring assignment
+// console.log(x, y, z);
+//
+// let [main, , secondary] = restaurant.categories; // skip element in the middle
+// console.log(main, secondary);
+//
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp; // changing main and secondary
+// // console.log(main, secondary);
+//
+// [main, secondary] = [secondary, main]; // do not need a temporary
+// console.log(main, secondary);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+//
+// // nested destructuring
+// const nested = [2, 3, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log(i, j);
+// const [i, , [j, k]] = nested;
+//
+// console.log(i, j, k);
+// ============================================================================================================
+// const { name, openingHours, categories } = restaurant; // creates new variables
+// console.log(name, openingHours, categories);
+//
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+//
+// console.log(restaurantName, hours, tags);
+//
+// const { menu = [], starterMenu: starters = [] } = restaurant; // giving starterMenu a new name with a default value incase it is undefined
+//
+// console.log(menu, starters);
+//
+// // mutating variabeles
+//
+// let a = 111;
+// let b = 999;
+//
+// const obj = { a: 23, b: 7, c: 14 };
+//
+// ({ a, b } = obj);
+// console.log(a, b);
+//
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+// ============================================================================================================
